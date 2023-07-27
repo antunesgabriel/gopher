53f1618a0b7fb91332e5afd400c308b574529c88
Antunes Gabriel <gabrielantunescontato@gmail.com>
2023-07-27 20:01:20 -0300

    chore: add readme and change structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gopher
+Gopher is a cli to help you start your go application using a template.
--- cmd/gopher/main.go
+++ cmd/gopher/main.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2023 Antunes <gabrielantunescontato@gmail.com>
 */
 package main
 
